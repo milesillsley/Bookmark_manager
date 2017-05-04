1c32db29ad12dd4232c7d05ca6d78f7d019a70fd
milesillsley <milesillsley@hotmail.co.uk>
2017-05-04 14:02:01 +0100

    Pass tagging tests

--- app/app.rb
+++ app/app.rb
@@ -17,7 +17,10 @@ class BookmarkManager < Sinatra::Base
     end
 
     post '/links' do
-      Link.create(url: params[:url], title: params[:title])
+      link = Link.create(url: params[:url], title: params[:title])
+      tag  = Tag.first_or_create(name: params[:tag])
+      link.tags << tag
+      link.save
       redirect '/links'
     end
 
